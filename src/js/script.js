fac6e1596793a79ececd21e141d5f3d26bdd24ef
Giovanna Dias <giovannaofdias@gmail.com>
2025-11-01 16:16:33 -0300

    5-Criando slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,28 @@
 function trocar(cor) {
     document.body.style.backgroundColor = cor;
 }
+
+let imagens=[
+    "src/assets/image1.jpg",
+    "src/assets/image2.jpg",
+    "src/assets/image3.webp",
+    "src/assets/image4.jpg",
+    "src/assets/image5.jpg"
+]
+
+//DECLARANDO AS VARIAVEIS
+
+let i = 0;
+let tempo = 5000; // Milisegundos
+
+// FUNÇÃO
+
+function slideShow(){
+    document.getElementById("image").src=imagens[i];
+    i++;
+    if(i == imagens.length){
+        i = 0;
+    }
+    setTimeout(slideShow,tempo)
+}
+slideShow();
